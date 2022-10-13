a8ff66d424bc5629c634c29053b4dea6a7a7de0e
root <root@ist351-fall2022-jarretts-lamp-vm.us-east1-b.c.weighty-treat-361718.internal>
2022-10-13 18:39:59 +0000

    Added PHP database info

--- createAccount.php
+++ createAccount.php
@@ -1,4 +1,52 @@
-<!DOCTYPE html>
+<?php
+
+	session_start();
+	if (isset($_POST['password1']))
+	{
+		if ($_POST['password1'] == $_POST['password2'])
+		{
+			$md5Pass = md5($_POST['password1']);
+			echo 'Password check.  Hash to: ' . $md5Pass . "<br>";
+			// insert database info here
+			$con = new mysqli('localhost','root','eBMcvmNiPRx7Wi','lampusers');
+			if ($con->connect_error)
+  			{
+  				die('Could not connect to mySQL: ' . $con->connect_error);
+  			}
+			//database insertion
+			$sql = "INSERT INTO cryptousers (Username, Password) VALUES ('$_POST[Username]', '$_POST[Password]', '$md5Pass')";
+			echo "Built sql: " . $sql;
+
+	        	if (!$con->query($sql)=== TRUE)
+       		   	{
+       		   		die('Error adding User: ' . $con->error);
+			}
+			//change this to have the mining page
+			header("Location: https://mercer.edu");
+       	 	}
+
+
+		else
+			{
+			die('Passwords do not match');
+			}
+	}
+	else
+	{
+  		echo "Send me the data";
+	}
+
+
+
+
+
+
+
+
+
+
+?>
+
 <html>
 	<head>
 		<title>Create Your Account</title>
