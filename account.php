6c05dee0449b75fdc2fa1b625c5765afdf9c4054
root <root@ist351-fall2022-jarretts-lamp-vm.us-east1-b.c.weighty-treat-361718.internal>
2022-10-17 21:34:34 +0000

    started create account page

---
+++ account.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Create Your Account!</title>
+</head>
+<body>
+<h1>Create User Account</h1>
+
+
+<form method="post" action="createUser.php">
+  First name:<br>
+  <input type="text" name="firstname"><br>
+  Last name:<br>
+  <input type="text" name="lastname" ><br><br>
+  User name:<br>
+  <input type="text" name="username" ><br><br>
+  Password:<br>
+  <input type="password" name="password1" ><br><br>
+  Re-enter your Password:<br>
+  <input type="password" name="password2" ><br><br>
+  <input type="submit" value="Create Account">
+</form>
+<style>
+form{
+	text-align: center;
+}
+input{
+	width: 75%;
+}
+h1{
+	text-align: center;
+}
+
+
+</style>
+</body>
+</html>
+
