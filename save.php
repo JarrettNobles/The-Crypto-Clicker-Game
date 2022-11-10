795a219cc978e582789dd5aa997d3184973fdad4
Jonathan C. Warren <warren_jc@cs.mercer.edu>
2022-11-10 14:38:38 -0500

    text box5

--- save.php
+++ save.php
@@ -1,18 +1,9 @@
 <?php
 
 	session_start();
-	if (isset($_POST['password1']))
+	if (isset($_POST['score']))
 	{
-		if ($_POST['password1'] == $_POST['password2'])
-		{
-			$md5Pass = md5($_POST['password1']);
-			echo 'Password check.  Hash to: ' . $md5Pass . "<br>";
-			// insert database info here
-			$con = new mysqli('localhost','root','XPkWWvhWzACj3q','cryptousers');
-			if ($con->connect_error)
-  			{
-  				die('Could not connect to mySQL: ' . $con->connect_error);
-  			}
+		
 			//database insertion
 			$sql = "INSERT INTO Users (score) VALUES ('$_POST[score]')";
 			echo "Built sql: " . $sql;
