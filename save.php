fa4f9e9fa32f3fb4f67b6f98ac3d7a6a54b04342
Jonathan C. Warren <warren_jc@cs.mercer.edu>
2022-11-10 14:43:26 -0500

    text box5

--- save.php
+++ save.php
@@ -3,7 +3,13 @@
 	session_start();
 	if (isset($_POST['score']))
 	{
-		
+		if ($_POST['score'] >= 1)
+		{
+			$con = new mysqli('localhost','root','XPkWWvhWzACj3q','cryptousers');
+			if ($con->connect_error)
+  			{
+  				die('Could not connect to mySQL: ' . $con->connect_error);
+  			}
 			//database insertion
 			$sql = "INSERT INTO Users (score) VALUES ('$_POST[score]')";
 			echo "Built sql: " . $sql;
@@ -12,6 +18,7 @@
        		   	{
        		   		die('Error adding User: ' . $con->error);
 			}
+		}
 			//change this to have the mining page
 			header("Location: game.php");
        	 	}
